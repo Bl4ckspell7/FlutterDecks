081b3a8e27e70098351d1b8dccadd19f0829aa37
Bl4ckspell7 <bl4ckspell@proton.me>
2025-03-18 19:44:13 +0100

    add test list widget

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
             useMaterial3: true,
           ),
           themeMode: ThemeMode.system,
-          home: const NavigationExample(),
+          home: const ListScrollTest(),
         );
       },
     );
@@ -123,6 +123,36 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ListScrollTest extends StatefulWidget {
+  const ListScrollTest({super.key});
+
+  @override
+  State<ListScrollTest> createState() => _ListScrollTestState();
+}
+
+class _ListScrollTestState extends State<ListScrollTest> {
+  final List<int> _items = List<int>.generate(100, (int index) => index);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: _items.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text('Item $index'),
+            onTap: () {
+              setState(() {
+                _items.removeAt(index);
+              });
+            },
+          );
+        },
+      ),
     );
   }
 }
