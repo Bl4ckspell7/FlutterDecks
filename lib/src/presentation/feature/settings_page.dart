50e97b653d8653a7a80521f714c984c26bb0a524
Bl4ckspell <bl4ckspell@proton.me>
2025-09-03 18:35:26 +0200

    remove list_scroll_test

--- lib/src/presentation/feature/settings_page.dart
+++ lib/src/presentation/feature/settings_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_decks/src/presentation/feature/list_scroll_test.dart'
-    show ListScrollTest;
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -12,9 +10,6 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Settings Page")),
-      body: ListScrollTest(listSize: 50),
-    );
+    return const Scaffold(body: Center(child: Text('Settings Page')));
   }
 }
